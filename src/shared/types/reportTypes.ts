e90a025520cc506f5e4c2520f52b8f901d77d27d
suin <73993670+whl5105@users.noreply.github.com>
2025-09-07 17:29:56 +0900

    feat: 사고 리포트 페이지 UI &  API 연결  #6

---
+++ src/shared/types/reportTypes.ts
@@ -0,0 +1,54 @@
+import type { ApiResponse } from '@shared-types/apiResponse';
+
+export type ScaleType = 'medium' | 'high'; // medium(심각) | high(보통)
+
+interface drivingLogs {
+  speed: number; // 차량 속도 (km/h)
+  decelerationRate: number; // 순간 감속 값 (m/s^2)
+  brakePedal: boolean; // 브레이크 상태 (true: 밟음, false: 안 밟음)
+  acceleratorPedal: boolean; // 엑셀 상태 (true: 밟음, false: 안 밟음)
+  timeOffsetSeconds: number; // 사고 시점 기준 상대 초 ( -10 .. 0 )
+}
+
+export interface Report {
+  accidentId: string; // 사고 아이디
+  user: {
+    userId: string; //유저 아이디
+    userName: string; //유저 이름
+  };
+  location: {
+    latitude: number; // 사고 발생 위도
+    longitude: number; // 사고 발생 경도
+  };
+  accidentAt: string; // 사고 발생 시각
+  deceleration: {
+    //감속 정보
+    decel: number; // 감속도 (m/s^2, 음수값일 수 있음)
+    startSpeed: number; //처음 스피드
+    endSpeed: number; //나중 스피드
+  };
+  emergencyResponse: {
+    // 응급 정보 섹션
+    emergencyNotified: boolean; //119 신고 유무
+    familyNotified: boolean; //가족 문자 전송 유무
+    reportedAt: string; // 119 신고 시각
+  };
+  impulse: number; //충격량
+  scale: ScaleType;
+  drivingLogs: drivingLogs[];
+}
+
+export type PagedResponse<T> = {
+  content: T[];
+  page: number;
+  totalPages: number;
+};
+
+export type ReportResponse = ApiResponse<PagedResponse<Report>>;
+
+export type ConfirmReportResponse = {
+  success: boolean;
+  code: number;
+  message: string;
+  data: null;
+};
